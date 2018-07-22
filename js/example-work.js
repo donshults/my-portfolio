d8280f07b1590356011d53b354e6981cbf6f41c2
Don Shults <don@tektanium.com>
2018-07-22 13:20:32 -0700

    update example work

---
+++ js/example-work.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class ExampleWork extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <section>It works</section>
+         );
+    }
+}
+ 
+export default ExampleWork;
--- js/main.js
+++ js/main.js
@@ -1,2 +1,5 @@
-import 'react-dom'
-console.log("loaded react-dom");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
+
+ReactDOM.render(<ExampleWork />, document.getElementById('example-work'));
