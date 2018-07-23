4f910e217834b3b04d12efadae5c9f03b9a5adb7
Don Shults <don@tektanium.com>
2018-07-23 10:58:40 -0700

    React Config

---
+++ __tests__/test-example-work.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import ExampleWork from '../js/example-work';
+
+describe("ExampleWork component", () => {
+    it("Should be a section element", () => {
+        expect("Hello").toEqual("Hello");
+    });
+});
--- js/main.js
+++ js/main.js
@@ -2,4 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ExampleWork from './example-work';
 
-ReactDOM.render(<ExampleWork />, document.getElementById('example-work'));
+const myWork =[
+    {
+        'title': 'Burger Builder using React1',
+        'href': 'https://example.com',
+        'desc': 'Some General Info',
+        'image': {
+            'desc': 'ReactJS Based App using Firebase for Data and Authentication',
+            'src': 'images/BurgerBuilderSnap.png',
+            'comment': 'Burger Builder Snap'
+        }
+    },
+    {
+        'title': 'Burger Builder using React2',
+        'href': 'https://example.com',
+        'desc': 'Some General Info',
+        'image': {
+            'desc': 'ReactJS Based App using Firebase for Data and Authentication',
+            'src': 'images/BurgerBuilderSnap.png',
+            'comment': 'Burger Builder Snap'
+        }
+    },
+    {
+        'title': 'Burger Builder using React3',
+        'href': 'https://example.com',
+        'desc': 'Some General Info',
+        'image': {
+            'desc': 'ReactJS Based App using Firebase for Data and Authentication',
+            'src': 'images/BurgerBuilderSnap.png',
+            'comment': 'Burger Builder Snap'
+        }
+    }
+]
+
+ReactDOM.render(<ExampleWork work={myWork} />, document.getElementById('example-work'));
